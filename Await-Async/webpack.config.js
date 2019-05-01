24c0877aa178ad0575a905dfe6f2ed7117c823ab
GQSM <ms314006@hotmail.com>
2019-05-01 15:08:54 +0800

    [CHANGE] Remove comment

--- Await-Async/webpack.config.js
+++ Await-Async/webpack.config.js
@@ -6,9 +6,7 @@ module.exports = {
     filename: 'dist/bundle.js',
     path: path.resolve(__dirname, './'),
   },
-  // 將loader的設定寫在module的rules屬性中
   module: {
-    // rules的值是一個陣列可以存放多個loader物件
     rules: [
       { test: /.js$/, exclude: /node_modules/, use: { loader: 'babel-loader', options: { presets: ['@babel/preset-env'], }, }, }
     ],
