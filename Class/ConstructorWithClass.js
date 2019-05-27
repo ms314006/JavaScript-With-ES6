217a7e2fdc9cbda8c1c4df6d6f1e918230b67e71
GQSM <ms314006@hotmail.com>
2019-05-27 22:28:10 +0800

    [ADD] 增加 static 和 getter & setter 的例子

--- Class/ConstructorWithClass.js
+++ Class/ConstructorWithClass.js
@@ -1,26 +1,57 @@
 class Person {
   constructor(name) {
     this.name = name;
+  }
+
+  static sayHello(name) {
+    return `Hi!${name}!`;
+  }
+
+  get age() {
+    if (this._age !== undefined) {
+      return `${this.name} age is ${this._age}.`;
+    }
+    return `Don't know ${this.name}'s age.`;
+  }
+
+  set age(age) {
+    this._age = age;
+  }
+
+  getFrom() {
     const state = 'Taiwan';
-    this.getFrom = () => `${this.name} from ${state}.`;
+    return `${this.name} from ${state}.`;
   }
 }
+
+class Employee extends Person {
+  constructor(name, position) {
+    super(name);
+    this.position = position;
+  }
+
+  getPosition() {
+    return `${this.name}'s position is the ${this.position}.`;
+  }
+
+  superCallGetForm() {
+    return super.getFrom();
+  }
+}
+
+console.log(Person.sayHello('Luck'));
 
 const john = new Person('John');
 
 console.log(JSON.stringify(john));
 console.log(JSON.stringify(john.state));
 console.log(JSON.stringify(john.getFrom()));
-
-class Employee extends Person {
-  constructor(name, position) {
-    super(name);
-    this.position = position;
-    this.getPosition = () => `${this.name}'s position is the ${this.position}.`;
-  }
-}
+console.log(john.age);
+console.log(john.age = 18);
+console.log(john.age);
 
 const luck = new Employee('Luck', 'Front-end');
 
 console.log(luck.getFrom());
 console.log(luck.getPosition());
+console.log(luck.superCallGetForm());
